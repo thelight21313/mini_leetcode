48458cc670d61e155bca043c965d0eb76d7aac54
thelight21313 <artemijpopkov6@gmail.com>
2026-03-30 21:49:25 +0400

    fix

--- leet_code/settings.py
+++ leet_code/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-app', 'nginx']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-app', 'nginx', 'nenado-lit.ru', 'www.nenado-lit.ru']
 
 
 # Application definition
