73426f184e8eaeb8b5f602bfcea726ae1d1b286d
thelight21313 <artemijpopkov6@gmail.com>
2026-03-30 20:21:54 +0400

    fix

--- leet_code/settings.py
+++ leet_code/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i@y(tt^u)aak8o4b0s^_v(awon$wptfu+)i$9$nv8$@26@qz@s'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
